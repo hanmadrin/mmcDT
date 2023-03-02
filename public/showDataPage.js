719c3d092df756eda848149a5d5d9fd75d9728fb
Ashik Ahmed Fahim <ashikahmed96@gmail.com>
2023-03-02 15:45:29 +0600

    data show css updated

--- public/showDataPage.js
+++ public/showDataPage.js
@@ -2,95 +2,76 @@ const showDataPage = (data) => {
     const body = document.querySelector('body');
     const showDataPage = document.createElement('div');
     showDataPage.classList.add('show-data-page');
-    const container = document.createElement('div');
-    container.classList.add('container');
-    //show pdf file 
-    const showPdfFile = document.createElement('embed');
-    showPdfFile.classList.add('show-pdf-file');
-    showPdfFile.setAttribute('src', `/${data.file}`);
-    showPdfFile.setAttribute('width', '100%');
-    showPdfFile.setAttribute('height', '100vh');
-    showPdfFile.setAttribute('type', 'application/pdf');
-    showPdfFile.setAttribute('scrolling', 'no');
-    //show data
-    const showData = document.createElement('div');
-    showData.classList.add('show-data');
+    const showDataContent = document.createElement('div');
+    showDataContent.classList.add('show-data-content');
     //header
     const showHeader = document.createElement('div');
     showHeader.classList.add('show-header');
     showHeader.innerHTML = `
         <div>
             <p>
-                <span>Invoice Number</span>
-                <span>${data.response.header['Invoice Number']}</span>
+                <span>Invoice Number</span> -
+                <span class='api-data'>${data.response.header['Invoice Number']}</span>
             </p>
             <p>
-                <span>Vendor</span>
-                <span>${data.response.header['Vendor']}</span>
+                <span>Vendor</span> -
+                <span class='api-data'>${data.response.header['Vendor']}</span>
             </p>
             <p>
-                <span>Phone</span>
-                <span>${data.response.header['Phone']}</span>
+                <span>Phone</span> -
+                <span class='api-data'>${data.response.header['Phone']}</span>
             </p>
             <p>
-                <span>Fax</span>
-                <span>${data.response.header['Fax']}</span>
+                <span>Fax</span> -
+                <span class='api-data'>${data.response.header['Fax']}</span>
             </p>
             <p>
-                <span>Insurance</span>
-                <span>${data.response.header['Insurance']}</span>
+                <span>Insurance</span> -
+                <span class='api-data'>${data.response.header['Insurance']}</span>
             </p>
             <p>
-                <span>Claim Number</span>
-                <span>${data.response.header['Claim Number']}</span>
+                <span>Claim Number</span> -
+                <span class='api-data'>${data.response.header['Claim Number']}</span>
             </p>
         </div>
         <div>
             <p>
-                <span>Invoice Date</span>
-                <span>${data.response.header['Invoice Date']}</span>
+                <span>Invoice Date</span> -
+                <span class='api-data'>${data.response.header['Invoice Date']}</span>
             </p>
             <p>
-                <span>RO Number</span>
-                <span>${data.response.header['RO Number']}</span>
+                <span>RO Number</span> -
+                <span class='api-data'>${data.response.header['RO Number']}</span>
             </p>
             <p>
-                <span>Discount %</span>
-                <span>${data.response.header['Discount %']}</span>
+                <span>Discount %</span> -
+                <span class='api-data'>${data.response.header['Discount %']}</span>
+            </p> class='api-data'
+            <p>
+                <span>Payment Type</span> -
+                <span class='api-data'>${data.response.header['Payment Type']}</span>
             </p>
             <p>
-                <span>Payment Type</span>
-                <span>${data.response.header['Payment Type']}</span>
-            </p>
-            <p>
-                <span> Check/Auth. Number</span>
-                <span>${data.response.header['Check/Auth. Number']}</span>
+                <span> Check/Auth. Number</span> -
+                <span class='api-data'>${data.response.header['Check/Auth. Number']}</span>
             </p>
         </div>
     `;
     //body
     const showBody = document.createElement('div');
     showBody.classList.add('show-body');
-    showBody.innerHTML = `
-        <table>
-            <tr>
+    showBody.innerHTML = `<table>
+            <tr class='border-bottom'>
                 <th>Line</th>
                 <th>Part Number</th>
                 <th>Part Type</th>
                 <th>RO List $</th>
                 <th>RO Sales $</th>
-                <th>Received</th>
-                <th>Invoice</th>
-                <th>Unit Cost</th>
-                <th>Discount</th>
-                <th>Extended</th>
-            </tr>
-            <tr class='table-second-header'>
-                <th colspan='6'>Qty</th>
-                <th>List $</th>
-                <th>$</th>
-                <th>%</th>
-                <th>Cost $</th>
+                <th>Received Qty</th>
+                <th>Invoice List $</th>
+                <th>Unit Cost $</th>
+                <th>Discount %</th>
+                <th>Extended Cost $</th>
             </tr>
             ${data.response.body.map((item, index) => { 
                 return `
@@ -98,7 +79,7 @@ const showDataPage = (data) => {
                         <td>${item['Line']}</td>
                         <td colspan='9'>${item['Description']}</td>
                     </tr>
-                    <tr class='table-second-body'>
+                    <tr class='table-second-body border-bottom'>
                         <td></td>
                         <td>${item['Part Number']}</td>
                         <td>${item['Part Type']}</td>
@@ -120,50 +101,31 @@ const showDataPage = (data) => {
     showFooter.innerHTML = `
         <div>
             <p>
-                <span>Received Items</span>
+                <span>Received Items</span> -
+                <span class='api-data'>${data.response.footer['Received Items']}</span>
             </p>
             <p>
-                <span>Subtotal</span>
+                <span>Subtotal</span> -
+                <span class='api-data'>${data.response.footer['Subtotal']}</span>
             </p>
             <p>
-                <span>Tax</span>
+                <span>Tax</span> -
+                <span class='api-data'>${data.response.footer['Tax']}</span>
             </p>
             <p>
-                <span>Freight</span>
+                <span>Freight</span> -
+                <span class='api-data'>${data.response.footer['Freight']}</span>
             </p>
             <p>
-                <span>Grand Total</span>
-            </p>
-        </div>
-        <div>
-            <p>
-                <span>${data.response.footer['Received Items']}</span>
-            </p>
-        </div>
-        <div>
-            <p></p>
-            </br>
-            <p>
-                <span>${data.response.footer['Subtotal']}</span>
-            </p>
-            <p>
-                <span>${data.response.footer['Tax']}</span>
-            </p>
-            <p>
-                <span>${data.response.footer['Freight']}</span>
-            </p>
-            <p>
-                <span>$${data.response.footer['Grand Total']}</span>
+                <span>Grand Total</span> -
+                <span class='api-data'>$${data.response.footer['Grand Total']}</span>
             </p>
         </div>
     `;
-    showData.appendChild(showHeader);
-    showData.appendChild(showBody);
-    showData.appendChild(showFooter);
-    //append 
-    container.appendChild(showPdfFile);
-    container.appendChild(showData);
-    showDataPage.appendChild(container);
+    showDataContent.appendChild(showHeader);
+    showDataContent.appendChild(showBody);
+    showDataContent.appendChild(showFooter);
+    showDataPage.appendChild(showDataContent);
     body.appendChild(showDataPage);
 };
 
