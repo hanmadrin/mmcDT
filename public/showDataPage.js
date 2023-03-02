a1c96f9c5383bbdca66915ae227149a11023a7da
Ashik Ahmed Fahim <ashikahmed96@gmail.com>
2023-03-02 17:54:04 +0600

    data showing completed

--- public/showDataPage.js
+++ public/showDataPage.js
@@ -1,127 +1,78 @@
+import uploadPdfPage from "./uploadPdfPage.js";
 const showDataPage = (data) => {
     const body = document.querySelector('body');
     const showDataPage = document.createElement('div');
     showDataPage.classList.add('show-data-page');
     const showDataContent = document.createElement('div');
     showDataContent.classList.add('show-data-content');
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('sticky');
+    const backButton = document.createElement('button');
+    backButton.classList.add('back-button');
+    backButton.innerText = 'Cancel';
+    const backToUpload = () => {
+        body.removeChild(showDataPage);
+        uploadPdfPage();
+    };
+    backButton.addEventListener('click', backToUpload);
+    const confirmButton = document.createElement('button');
+    confirmButton.classList.add('confirm-button');
+    confirmButton.innerText = 'Confirm';
+    buttonDiv.appendChild(backButton);
+    buttonDiv.appendChild(confirmButton);
     //header
     const showHeader = document.createElement('div');
     showHeader.classList.add('show-header');
-    showHeader.innerHTML = `
-        <div>
-            <p>
-                <span>Invoice Number</span> -
-                <span class='api-data'>${data.response.header['Invoice Number']}</span>
-            </p>
-            <p>
-                <span>Vendor</span> -
-                <span class='api-data'>${data.response.header['Vendor']}</span>
-            </p>
-            <p>
-                <span>Phone</span> -
-                <span class='api-data'>${data.response.header['Phone']}</span>
-            </p>
-            <p>
-                <span>Fax</span> -
-                <span class='api-data'>${data.response.header['Fax']}</span>
-            </p>
-            <p>
-                <span>Insurance</span> -
-                <span class='api-data'>${data.response.header['Insurance']}</span>
-            </p>
-            <p>
-                <span>Claim Number</span> -
-                <span class='api-data'>${data.response.header['Claim Number']}</span>
-            </p>
-        </div>
-        <div>
-            <p>
-                <span>Invoice Date</span> -
-                <span class='api-data'>${data.response.header['Invoice Date']}</span>
-            </p>
-            <p>
-                <span>RO Number</span> -
-                <span class='api-data'>${data.response.header['RO Number']}</span>
-            </p>
-            <p>
-                <span>Discount %</span> -
-                <span class='api-data'>${data.response.header['Discount %']}</span>
-            </p> class='api-data'
-            <p>
-                <span>Payment Type</span> -
-                <span class='api-data'>${data.response.header['Payment Type']}</span>
-            </p>
-            <p>
-                <span> Check/Auth. Number</span> -
-                <span class='api-data'>${data.response.header['Check/Auth. Number']}</span>
-            </p>
-        </div>
-    `;
+    Object.keys(data.response.header).forEach((item) => {
+        const p = document.createElement('p');
+        const span = document.createElement('span');
+        span.innerText = `${item} - `;
+        p.appendChild(span);
+        const span2 = document.createElement('span');
+        span2.innerText = data.response.header[item];
+        span2.classList.add('api-data');
+        p.appendChild(span2);
+        showHeader.appendChild(p);
+    });
     //body
     const showBody = document.createElement('div');
     showBody.classList.add('show-body');
-    showBody.innerHTML = `<table>
-            <tr class='border-bottom'>
-                <th>Line</th>
-                <th>Part Number</th>
-                <th>Part Type</th>
-                <th>RO List $</th>
-                <th>RO Sales $</th>
-                <th>Received Qty</th>
-                <th>Invoice List $</th>
-                <th>Unit Cost $</th>
-                <th>Discount %</th>
-                <th>Extended Cost $</th>
-            </tr>
-            ${data.response.body.map((item, index) => { 
-                return `
-                    <tr>
-                        <td>${item['Line']}</td>
-                        <td colspan='9'>${item['Description']}</td>
-                    </tr>
-                    <tr class='table-second-body border-bottom'>
-                        <td></td>
-                        <td>${item['Part Number']}</td>
-                        <td>${item['Part Type']}</td>
-                        <td class='text-right'>${item['RO List $']}</td>
-                        <td class='text-right'>${item['RO Sales $']}</td>
-                        <td class='text-right'>${item['Received Qty']}</td>
-                        <td class='text-right'>${item['Invoice List $']}</td>
-                        <td class='text-right'>${item['Unit Cost $']}</td>
-                        <td class='text-right'>${item['Discount %']}</td>
-                        <td class='text-right'>${item['Extended Cost $']}</td>
-                    </tr>
-                `;
-            })}
-        </table>
-    `;
+    const table = document.createElement('table');
+    const tr = document.createElement('tr');
+    data.response.body.forEach((item, index) => {
+        if (index > 0) return;
+        Object.keys(item).forEach((key) => {
+            const th = document.createElement('th');
+            th.innerText = key;
+            tr.appendChild(th);
+        });
+    });
+    table.appendChild(tr);
+    data.response.body.forEach((item) => {
+        const dataTr = document.createElement('tr');
+        Object.keys(item).forEach((key) => {
+            const td = document.createElement('td');
+            td.innerText = item[key];
+            dataTr.appendChild(td);
+        });
+        table.appendChild(dataTr);
+    })
+    showBody.appendChild(table);
     //footer 
     const showFooter = document.createElement('div');
     showFooter.classList.add('show-footer');
-    showFooter.innerHTML = `
-        <div>
-            <p>
-                <span>Received Items</span> -
-                <span class='api-data'>${data.response.footer['Received Items']}</span>
-            </p>
-            <p>
-                <span>Subtotal</span> -
-                <span class='api-data'>${data.response.footer['Subtotal']}</span>
-            </p>
-            <p>
-                <span>Tax</span> -
-                <span class='api-data'>${data.response.footer['Tax']}</span>
-            </p>
-            <p>
-                <span>Freight</span> -
-                <span class='api-data'>${data.response.footer['Freight']}</span>
-            </p>
-            <p>
-                <span>Grand Total</span> -
-                <span class='api-data'>$${data.response.footer['Grand Total']}</span>
-            </p>
-        </div>
-    `;
+    Object.keys(data.response.footer).forEach((item) => {
+        const p = document.createElement('p');
+        const span = document.createElement('span');
+        span.innerText = `${item} - `;
+        p.appendChild(span);
+        const span2 = document.createElement('span');
+        span2.innerText = data.response.footer[item];
+        span2.classList.add('api-data');
+        p.appendChild(span2);
+        showFooter.appendChild(p);
+    });
+    showDataContent.appendChild(buttonDiv);
     showDataContent.appendChild(showHeader);
     showDataContent.appendChild(showBody);
     showDataContent.appendChild(showFooter);
