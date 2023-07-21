b4d6579aafba32f51d732edb5a52724c5d2ccc1b
hanmadrin <hanmadrin@gmail.com>
2023-07-21 15:05:57 +0600

    file error solved

--- utilities/pdfToText.js
+++ utilities/pdfToText.js
@@ -50,6 +50,7 @@ module.exports.pdfToText = async (pdfPath) => {
         //     console.log('The file has been saved!');
         // });
         fileData = fileData.replace(/(\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{1,2}:\d{1,2}\s[A|P]M)@@@Page\s\d{1,2}\n\n\nMATTHEWS MOTOR COMPANY\n/g,'');
+        fs.writeFileSync('test.txt', fileData);
         const header = (() => {
             const headerContentStructure = [
                 {
@@ -110,7 +111,7 @@ module.exports.pdfToText = async (pdfPath) => {
             //         17   Bumper w/o Iron Man pkg w/fog lamps
             // 86510J9000    OEM    275.38    275.38    1    275.38    232.55    15.6    232.55
             // regex
-            const bodyLineRegex = /([\d-]){1,3}[\s@]{2,4}([A-z\s,/()"'.0-9\&-]{4,})\n([~-\w]+)(([@\s]{2,4})|\n)([\w\s]+)[@\s]{2,4}([\d,.]+)[@\s]{2,4}[-\d.,]+[@\s]{2,4}([\d,.]+)[@\s]{2,4}([\d,.]+)[@\s]{2,4}([\d,.]+)[@\s]{2,4}([\d,.-]+)[@\s]{2,4}([\d,.]+)\n/g;
+            const bodyLineRegex = /([\d-]){1,3}[\s@]{2,4}([A-z\s,/()"'.0-9\&-]{4,})\n([~-\w\/]+)(([@\s]{2,4})|\n)([\w\s-]+)[@\s]{2,4}([\d,.]+)[@\s]{2,4}[-\d.,]+[@\s]{2,4}([\d,.]+)[@\s]{2,4}([\d,.]+)[@\s]{2,4}([\d,.]+)[@\s]{2,4}([\d,.-]+)[@\s]{2,4}([\d,.]+)\n/g;
             const bodyLines = fileData.match(bodyLineRegex);
             // create body object
             // console.log(bodyLines.length)
